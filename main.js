3127c027625f848b24de85d275ae96b410a549a9
Aizahzaman <79235506+Aizahzaman@users.noreply.github.com>
2021-06-28 22:03:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-var canvas= new fabric.Canvas("mycanvas");
+var canvas= new fabric.Canvas("myCanvas");
 
 ball_x=0;
 ball_y=0;
-hole_x=400;
-hole_y=800;
+hole_x=800;
+hole_y=400;
 
 
 block_image_width = 5;
@@ -26,7 +26,7 @@ function load_img(){
 
 function new_image()
 {
-	fabric.Image.fromURL("golf-h.png",function(Img)		{
+	fabric.Image.fromURL("ball.png",function(Img)		{
 		ball_obj=Img;
 		ball_obj.scaleToWidth(50);
 		ball_obj.scaleToHeight(50);
@@ -47,7 +47,7 @@ function my_keydown(e)
 	 if((ball_x==hole_x)&&(ball_y==hole_y)) {
 		 canvas.remove(ball_obj);
 		 document.getElementById("hd3").innerHTML="YOU HAVE HIT THE GOAAAALLLL!!!!!!";
-		 document.getElementById("mycanvas").style.borderColor="red";
+		 document.getElementById("myCanvas").style.borderColor="red";
 	 }
 	else{
 		if(keyPressed == '38')
@@ -85,7 +85,7 @@ function my_keydown(e)
 
 	function down()
 	{
-		if( ball_y>=450){
+		if( ball_y<=450){
 			ball_y=ball_y+block_image_height;
 			console.log("block image height = "+ block_image_height);
 			console.log("When down arrow key is pressed, X = "+ ball_x + ",Y = " + ball_y );
